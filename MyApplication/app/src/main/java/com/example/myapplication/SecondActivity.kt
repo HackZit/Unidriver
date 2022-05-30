e0ac77df6784500d9eaf6254eb3944d1f49b6093
sebaxe07 <sebaxe09@gmail.com>
2022-05-30 15:39:32 -0500

    Mapa

--- MyApplication/app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -1,17 +1,32 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class SecondActivity : AppCompatActivity() {
+// Implement OnMapReadyCallback.
+class SecondActivity: AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Set the layout file as the content view.
         setContentView(R.layout.activity_second)
 
-        val actionBar = supportActionBar
+        // Get a handle to the fragment and register the callback.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+    }
 
-        actionBar!!.title = "UniDriver"
-
-        actionBar.setDisplayHomeAsUpEnabled(true)
+    // Get a handle to the GoogleMap object and display marker.
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(0.0, 0.0))
+                .title("Marker")
+        )
     }
 }
