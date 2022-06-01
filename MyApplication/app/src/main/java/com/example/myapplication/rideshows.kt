1878da3b1f0b76d50a7c30070ae716038940bb60
carlos204658 <carlosotero@uninorte.edu.co>
2022-05-31 19:51:39 -0500

    Update rideshows.kt

--- MyApplication/app/src/main/java/com/example/myapplication/rideshows.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/rideshows.kt
@@ -1,13 +1,23 @@
 package com.example.myapplication
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_rideshows.*
 import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
 
 class rideshows : AppCompatActivity() {
+
+
     private val ip = "ec2-54-165-184-219.compute-1.amazonaws.com" // this is the host ip that your data base exists on you can use 10.0.2.2 for local host                                                    found on your pc. use if config for windows to find the ip if the database exists on                                                    your pc
 
     private val port = "5432" // the port sql server runs on
@@ -22,19 +32,54 @@ class rideshows : AppCompatActivity() {
 
     private val url = "jdbc:postgresql://$ip:$port/$database?ssl=true&sslmode=require&sslfactory=org.postgresql.ssl.NonValidatingFactory" // the connection url string
 
-
-
     private var connection: Connection? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rideshows)
+    }
 
-        val ride = rideshowev("uni","uninorte","12:39PM",R.drawable.coche)
 
-        val listaRides = listOf(ride)
 
-        val adapter = rideAdapter(this,listaRides)
-        lista.adapter = adapter
+
+    fun query(view: View?) {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.INTERNET),
+            PackageManager.PERMISSION_GRANTED
+        )
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+        try {
+            Class.forName(Classes)
+            connection = DriverManager.getConnection(url, username, password)
+            Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show()
+
+            var ride = rideshowev("test","test","test",R.drawable.coche)
+            var listaRides = listOf(ride)
+
+            val sql = "SELECT  * FROM viajes WHERE ACTIVO='true'"
+            val rs = connection?.createStatement()?.executeQuery(sql)
+            if (rs != null) {
+                rs.next()
+                var ride = rideshowev(rs.getString(2),rs.getString(3),rs.getString(8),R.drawable.coche)
+
+
+            } else {
+                Toast.makeText(this, "Es nulo", Toast.LENGTH_SHORT).show()
+            }
+
+            val adapter = rideAdapter(this,listaRides)
+            lista.adapter = adapter
+
+        } catch (e: ClassNotFoundException) {
+            e.printStackTrace()
+            Toast.makeText(this, "Class fail", Toast.LENGTH_SHORT).show()
+        } catch (e: SQLException) {
+            e.printStackTrace()
+            Toast.makeText(this, "Connected no " + e, Toast.LENGTH_LONG).show()
+        }
     }
 
     fun secondscreen(view: View?) {
